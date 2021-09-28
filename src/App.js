2b72cb10d58a6a47c2506088b48c7b178bb49d32
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-09-28 00:03:20 -0400

    installs React-Draggable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Mercury from "./components/Mercury";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
+        <Mercury />
       </header>
     </div>
   );
---
+++ src/components/Mercury.js
@@ -0,0 +1,12 @@
+import Draggable from "react-draggable";
+
+export default function Mercury() {
+  return (
+    <Draggable>
+      <div className="mercury">
+        <p>today is </p>
+        <p>Is Mercury In Retrograde? 🌙</p>
+      </div>
+    </Draggable>
+  );
+}
