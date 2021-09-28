82d70dc1eb0ceee03436d557dd9d81409c74608c
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-09-28 00:13:40 -0400

    adds date and formats in correctly for API

--- src/components/Mercury.js
+++ src/components/Mercury.js
@@ -1,6 +1,12 @@
 import Draggable from "react-draggable";
 
 export default function Mercury() {
+  const date = new Date();
+  console.log(date.toISOString().split("T")[0]);
+
+  function fetchMercury() {
+    fetch("");
+  }
   return (
     <Draggable>
       <div className="mercury">
